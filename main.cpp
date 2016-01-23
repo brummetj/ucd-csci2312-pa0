e8e1626c3942611097179822ccb2b8898e18a708
brummetj <joshua.brummet@ucdenver.edu>
2016-01-23 14:44:35 -0700

    First commit of completed pa0

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 /**
  * File: main.cpp
  * Project: ucd-csci2312-pa0
- * Author: Ivo Georgiev
+ * Author: Joshua Brummet
  * Date: 2016-01-18
  */
 #include <iostream>
 
 int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+    
+    std::cout << "Hello, world! My number is " << 4 << '!' << std::endl;
 
     return 0;
 }
